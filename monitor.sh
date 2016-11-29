80b1093386932538dbc0ab8ad5ccbf82497b3079
sf0x <fox.troiza@gmail.com>
2016-11-30 00:46:34 +0100

    u

--- monitor.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-inotifywait -m /home/f0x/testfolder/ -e create -e modify -e delete -e move -e close_write |
-	while read path action file; do
-		echo "The file '$file' in '$path' was changed"
-	done
-
